31e7c99a23925a53c88e0d7625a6165a8c39e804
Josh Centers <josh.centers@gmail.com>
2019-08-30 17:17:46 -0500

    Rewrote checkScene() and it actually works!

--- index.js
+++ index.js
@@ -16,36 +16,53 @@ const HOME_BUTTONS = document.querySelectorAll(".button");
 const SCENE_BUTTONS = document.querySelectorAll(".scene");
 const ACCESSORY_BUTTONS = document.querySelectorAll(".accessory");
 
+
 HOME_BUTTONS.forEach(button => {
     button.addEventListener("click", function () {
         if (this.classList.contains("accessory") && this.classList.contains("on") && !this.classList.contains("thermostat")) {
             this.lastElementChild.innerText = "Off";
             this.classList.toggle("on");
-            checkScene();
+            // checkScene();
+            betterCheckScene();
         } else if (this.classList.contains("accessory") && !this.classList.contains("thermostat")) {
             this.lastElementChild.innerText = "On";
             this.classList.toggle("on");
-            checkScene();
+            // checkScene();
+            betterCheckScene();
         } else if (this.classList.contains("scene")) {
-            this.classList.toggle("on");
-            setScene(this.id)
+            // this.classList.toggle("on");
+            setScene(this.id);
+            betterCheckScene();
         }
     });
 });
 
+
+// This is what you want to do when you click an accessory button
+// Turn that accessory on or off depending on its state
+// Check all the scenes to see if their associated accessories are on
+// If they are on, turn the scene on. If not, turn the scene off.
+
+// This is what you want to do when you click a scene button
+// Turn on the accessories tied to that scene
+// Check all the scenes to see if their associated accessories are on
+// If they are on, turn the scene on. If not, turn the scene off.
+
 function setScene(sceneID) {
+    //The problem is here. You're setting accessories based on the state of the scene button, when instead it should be 
+    document.getElementById(sceneID).classList.toggle("on");
     let sceneToBeSet = sceneAccessories[sceneID];
     if (document.getElementById(sceneID).classList.contains("on")) {
 
-        sceneToBeSet.forEach(scene => {
-            document.getElementById(scene).classList.add("on");
-            document.getElementById(scene).lastElementChild.innerText = "On";
+        sceneToBeSet.forEach(accessory => {
+            document.getElementById(accessory).classList.add("on");
+            document.getElementById(accessory).lastElementChild.innerText = "On";
         });
     } else {
 
-        sceneToBeSet.forEach(scene => {
-            document.getElementById(scene).classList.remove("on");
-            document.getElementById(scene).lastElementChild.innerText = "Off";
+        sceneToBeSet.forEach(accessory => {
+            document.getElementById(accessory).classList.remove("on");
+            document.getElementById(accessory).lastElementChild.innerText = "Off";
         });
     }
 }
@@ -58,13 +75,15 @@ function checkScene() {
 
     let activeAccessories = [];
 
-    ACCESSORY_BUTTONS.forEach(button => {
-        if (button.classList.contains("on")) {
-            activeAccessories.push(button.id);
+    ACCESSORY_BUTTONS.forEach(accessory => {
+        if (accessory.classList.contains("on")) {
+            activeAccessories.push(accessory.id);
         }
     });
 
     activeAccessories.sort();
+    let activeAccessoriesString = activeAccessories.toString();
+    // console.log(`activeAccessoriesString equals ${activeAccessoriesString}`);
 
     SCENE_BUTTONS.forEach(scene => {
         let sceneAccessoryList = sceneAccessories[scene.id];
@@ -73,13 +92,28 @@ function checkScene() {
 
         // This is my crappy workaround to compare the two arrays without writing a ton of extra code.
 
-        activeAccessoriesString = activeAccessories.toString();
-        sceneAccessoryString = sceneAccessoryList.toString();
+        let sceneAccessoryString = sceneAccessoryList.toString();
+        console.log(`sceneAccessoryString for ${scene} equals ${sceneAccessoryString}`);
 
         if (activeAccessoriesString === sceneAccessoryString) {
             document.getElementById(scene.id).classList.add("on");
         } else {
             document.getElementById(scene.id).classList.remove("on");
         }
+    });
+}
+
+function betterCheckScene() {
+    SCENE_BUTTONS.forEach(scene => { // Iterate through each scene
+        let sceneAccessoryArray = sceneAccessories[scene.id]; //Create a list of associated accessories for each scene
+        switch (sceneAccessoryArray.every((node) => document.getElementById([node]).classList.contains("on"))) {
+            case true:
+                document.getElementById(scene.id).classList.add("on");
+                break;
+            case false:
+                document.getElementById(scene.id).classList.remove("on");
+                break;
+        }
+
     });
 }
